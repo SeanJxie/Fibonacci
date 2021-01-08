089f2b31155379771ab882a8be06d6a295f61b02
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2021-01-08 14:40:32 -0500

    Update FibPrimes.py

--- FibPrimes.py
+++ FibPrimes.py
@@ -4,22 +4,14 @@ computed = [0, 1]
 
 '''Implement the Fermat Prime Test'''
 def FermatPrimeTest(number):
-    ''' if number != 1 '''
     if (number > 1):
-        ''' repeat the test few times '''
-        for time in range(3):
-            ''' Draw a RANDOM number in range of number ( Z_number )  '''
+        for _ in range(3):
             randomNumber = random.randint(2, number) - 1
-            
-            ''' Test if a^(n-1) = 1 mod n '''
             if (pow(randomNumber, number - 1, number) != 1):
                 return False
-        
         return True
     else:
-        ''' case number == 1 '''
-        return False  
-            
+        return False      
     return True
 
 
