8d4bc27720b631e91744a35b883717a9be48906c
Sean Xie <47431127+SeanJxie@users.noreply.github.com>
2021-04-14 11:52:02 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -2,28 +2,19 @@
 #include <time.h>
 
 /*
-Implementing the BINT struct
+Implementing the BINT struct by computing the Fibonacci sequence.
 */
 
 
 #define NTERMS 10000
 
 int main() {
-    /*BINT n1, n2;
-    bint_init(&n1, 1, "123125125");
-    bint_init(&n2, 1, "321124");
+    int nTerms;
 
-    bint_cpy(&n1, &n2);
+    printf("How many terms would you like to compute? (starting from term #1): ");
+    scanf_s("%d", &nTerms);
 
-    bint_putraw(&n1, 1);*/
-
-
-    // --- Fibonacci sequence ---
-    // About 14.2017 minutes to process the one millionth term.
-
-    printf("Generating Fibonacci sequence up to term %d.\n", NTERMS);
-
-    clock_t begin = clock();
+    printf("\Computing Fibonacci sequence up to term %d.\n", nTerms);
 
     FILE* fptr;
     fopen_s(&fptr, "Fibonacci_sequence.txt", "w");
@@ -33,41 +24,28 @@ int main() {
     BINT sum;
     double runtime = 0.0;
 
-    bint_init(&sum, 0, "0");
+    clock_t begin = clock();
 
-    // Initial t0 = 0 t1 = 1
+    bint_init(&sum, 0, "0");
     bint_init(&prev, 0, "0");
     bint_init(&curr, 1, "1");
 
     bint_writefile(fptr, 1, &curr, 1);
 
-    for (int t = 0; t < NTERMS - 1; t++) {
+    for (int t = 0; t < nTerms - 1; t++) {
         bint_add(&prev, &curr, &sum);
         bint_cpy(&prev, &curr);
         bint_cpy(&curr, &sum);
-        printf("\r%.2f%% complete.", (t + 1.0f) / (NTERMS - 1.0f) * 100.0f);
+        printf("\r%.2f%% complete.", (t + 1.0f) / (nTerms - 1.0f) * 100.0f);
         bint_writefile(fptr, t + 2, &curr, 1);
     }
 
     clock_t end = clock();
-
     runtime += (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("\nCalculation took %f seconds.", runtime);
+
+    printf("\n\nComputation took %f seconds.", runtime);
+
     if (fptr != NULL) { fclose(fptr); }
-
-    // --- With standard types ---
-    // unsigned long long int prev = 0;
-    // unsigned long long int curr = 1;
-    // unsigned long long int sum = 0;
-
-    // for (int _ = 0; _ < NTERMS; _++) {
-    //     printf("%d\n", curr);
-    //     sum = prev + curr;
-    //     prev = curr;
-    //     curr = sum;
-    // }
-
-    // printf("%d\n", curr);
 
     return 0;
 }
